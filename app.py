dd9cc2894db30ec8dee85ee1234f4a8c2fe5edd1
jjlinnn <jiajiedeng2024@outlook.com>
2025-04-06 06:36:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,8 @@ def add_player():
     return jsonify({"message": f"成功添加选手：{new_player['name']}"})
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
