86cb10f4130e65606ea6e67d7efe1b97d7485161
jiajiedeng <jiajiedeng2024@outlook.com>
2025-04-08 17:01:33 +0800

    login2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import request, redirect, url_for, render_template, flash
+from flask_admin.contrib.sqla import ModelView
 import random, os
 
 app = Flask(__name__)
